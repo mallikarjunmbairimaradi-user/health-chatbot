0fbe6f49c463f85ecce21ccd8af2a03cf18f8a97
Mallikarjun <mallikarjunmbairimaradi@gmail.com>
2026-04-09 21:48:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,14 @@ if "chat" not in st.session_state:
     st.session_state.chat = []
 
 user_input = st.chat_input("Ask your health question...")
-if st.button("Send"):
-    if user_input:
-        response = generate_response(user_input)
-        st.session_state.chat.append(("You", user_input))
-        st.session_state.chat.append(("Bot", response))
+
+if user_input:
+    response = generate_response(user_input)
+    st.session_state.chat.append(("You", user_input))
+    st.session_state.chat.append(("Bot", response))
 
 for sender, msg in st.session_state.chat:
     if sender == "You":
         st.chat_message("user").write(msg)
     else:
         st.chat_message("assistant").write(msg)
-        
